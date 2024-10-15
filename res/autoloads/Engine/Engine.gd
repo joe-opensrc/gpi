b50bbc0aec9ccdd937335a448091bee7ec4b3464
Joe OpenSrc <joe-opensrc@protonmail.ch>
2024-10-15 08:35:31 +0200

    missing var keyword

--- res/autoloads/Engine/Engine.gd
+++ res/autoloads/Engine/Engine.gd
@@ -1,7 +1,7 @@
 @tool
 extends Node
 
-@export settings: SettingsRes  # = preload("default_settings.tres")
+@export var settings: SettingsRes  # = preload("default_settings.tres")
 
 #func _ready() -> void:
 	#ProjectSettings.set("")
