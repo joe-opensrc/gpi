d2f6053f1daf31e22c84fc0dd416d508dfebffb8
Joe OpenSrc <joe-opensrc@protonmail.ch>
2024-10-16 23:33:17 +0200

    more WIP

--- res/autoloads/Director/Director.gd
+++ res/autoloads/Director/Director.gd
@@ -5,15 +5,47 @@ extends Node
 # A class to manage scene transitions and Other Things™ 
 # Might get split up later into Engine/Scene and Agents/AI, etc.
 
-class SceneTransition:
-	init( ):
-		@var
+
+# preLoadScene()
+# transitionNext()
+# transitionType() 
+
+# class SceneTransition:
+# 	init( ):
+
+# 
+#                root
+#	     _________ /  \ _______________
+#	    /         /    \         \     \
+#	 Enjyn    SigBus  Director  UIX  Realm2D
+
+
 
 #      ST:		
-#    Director   
+#	   Realm2D -- Enjyn
 #		   / \
 #		  .   x       y   -- pla 
 #    /   / \     / \
 #   .		.   .   .   .
 #                  / \
 #									.   . 
+
+
+	func addSceneTransition( self, prev, next, type ) 
+	signal reqSceneTransition( self, SceneTransition.Next ) => .connect( doSceneTransition )
+	func doSceneTransition( currentScene, dir: SceneTransitionDirection ):
+		transition_next
+
+	Enjyn.doSceneTransition(  )
+
+  SceneTransition:
+		prev: null
+		next: Node2D
+		type: Enum: shader|anim|
+		strict: false # pointers must be same type
+		next|prev():
+			if next|prev == null:	restart same scene
+			
+		- x: 
+
+		SceneTransition
