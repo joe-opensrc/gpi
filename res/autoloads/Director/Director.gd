17548101bae552052b6d3bc956d01118a4bfc6ab
Joe OpenSrc <joe-opensrc@protonmail.ch>
2024-10-16 18:42:28 +0200

    start of Director WIP

---
+++ res/autoloads/Director/Director.gd
@@ -0,0 +1,19 @@
+@tool
+extends Node
+
+###
+# A class to manage scene transitions and Other Things™ 
+# Might get split up later into Engine/Scene and Agents/AI, etc.
+
+class SceneTransition:
+	init( ):
+		@var
+
+#      ST:		
+#    Director   
+#		   / \
+#		  .   x       y   -- pla 
+#    /   / \     / \
+#   .		.   .   .   .
+#                  / \
+#									.   . 
