525b894e7bbfc0493bcada71c5df5862c085b2f8
Joe OpenSrc <joe-opensrc@protonmail.ch>
2024-10-29 13:29:30 +0100

    project setting defaults + fullScreen var

--- res/autoloads/Enjyn/SettingsRes.gd
+++ res/autoloads/Enjyn/SettingsRes.gd
@@ -1,9 +1,11 @@
 @tool
 class_name SettingsRes extends Resource
 
-@export var debug: bool			= false
-@export var rescale: bool   = true
-@export var resScale: float = 1.0
+@export var debug: bool			 = false
+@export var rescale: bool    = true
+@export var resScale: float  = 0.6 
+@export var fullScreen: bool = false
+
 
 # probably possible to overengineer with setget ;)
 @export var W: int = ProjectSettings.get_setting("display/window/size/viewport_width")
