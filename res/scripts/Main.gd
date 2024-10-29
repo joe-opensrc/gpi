b7684872433d1eb03f0c4b47332bfd5447d1187e
Joe OpenSrc <joe-opensrc@protonmail.ch>
2024-10-29 13:29:48 +0100

    thin out main

--- res/scripts/Main.gd
+++ res/scripts/Main.gd
@@ -4,22 +4,18 @@ func _init() -> void:
 	pass
 
 func _ready() -> void:
+	pass
 
-	if Enjyn.options.rescale:
-		Enjyn.rescaleWindow()
-
-func _unhandled_input(event: InputEvent) -> void:
-	#print(event)
-	if event.is_action_released("user_quit"):
-		engine_teardown()
-	
 func _process(delta: float) -> void:
 	pass
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_released("user_quit"):
+		engine_teardown()
 	
 func engine_teardown() -> void:
 	get_tree().quit()
 
-
 func _on_reload_pressed() -> void:
-	print("reloading...")
+	print("reloading...", self)
 	get_tree().reload_current_scene()
