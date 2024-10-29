1bebf2e16afd9234ecb5390aee0490b0d9b3d380
Joe OpenSrc <joe-opensrc@protonmail.ch>
2024-10-29 10:24:59 +0100

    scene based shenanigans moved to Enjyn from Director

--- res/autoloads/Enjyn/Enjyn.gd
+++ res/autoloads/Enjyn/Enjyn.gd
@@ -50,3 +50,79 @@ func _unhandled_input(event: InputEvent) -> void:
 		
 func engine_teardown() -> void:
 	get_tree().quit()
+
+# Scene based shenanigans
+func doSceneTransition( curScene, reqScene ):
+
+	var sceneExists = ResourceLoader.exists( reqScene )
+	var sceneCached = ResourceLoader.has_cached( reqScene )
+	var nextSceneRes: Resource
+	var nextScene: Node
+	
+	#print("DST: %s" % type_string( typeof( reqScene ) ) )
+	print("DST: RL: exists: %s, cached: %s" % [ sceneExists, sceneCached ] )
+	if sceneExists:
+		if not sceneCached:
+			nextSceneRes = ResourceLoader.load( reqScene )
+			nextScene = nextSceneRes.instantiate()
+			curScene.add_sibling.call_deferred( nextScene )
+	curScene.queue_free()
+
+	# if get_node_or_null(reqScene):
+		# print("ALREADY")
+	# else:
+		
+		#curScene.add_sibling( load(reqScene).instantiate() )
+	#match typeof( reqScene ):
+		#"String":
+		#"Node":
+		#"NodePath":
+		#TYPE_NODE_PATH
+
+# A class to manage scene transitions and Other Things™ 
+# Might get split up later into Engine/Scene and Agents/AI, etc.
+
+
+# preLoadScene()
+# transitionNext()
+# transitionType() 
+
+# class SceneTransition:
+# 	init( ):
+
+# 
+#                root
+#	     _________ /  \ _______________
+#	    /         /    \         \     \
+#	 Enjyn    SigBus  Director  UIX  Realm2D
+
+
+
+#      ST:
+#   Realm2D -- Enjyn
+#    / \
+#   .   x        y   -- pla 
+#  /   / \      / \
+# .   .   .    .   .  
+#    / \
+#   .   . 
+
+
+	#func addSceneTransition( self, prev, next, type ) 
+	#signal reqSceneTransition( self, SceneTransition.Next ) => .connect( doSceneTransition )
+	#func doSceneTransition( currentScene, dir: SceneTransitionDirection ):
+		#transition_next
+
+	#Enjyn.doSceneTransition(  )
+
+  #SceneTransition:
+		#prev: null
+		#next: Node2D
+		#type: Enum: shader|anim|
+		#strict: false # pointers must be same type
+		#next|prev():
+			#if next|prev == null:	restart same scene
+			#
+		#- x: 
+#
+		#SceneTransition
