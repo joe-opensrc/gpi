c1604b99647cc237fedbb5d989622760ea3dcf8b
Joe OpenSrc <joe-opensrc@protonmail.ch>
2024-10-30 10:20:47 +0100

    moved engine_teardown to Enjyn

--- res/scripts/Main.gd
+++ res/scripts/Main.gd
@@ -10,12 +10,8 @@ func _process(delta: float) -> void:
 	pass
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_released("user_quit"):
-		engine_teardown()
+	pass
 	
-func engine_teardown() -> void:
-	get_tree().quit()
-
 func _on_reload_pressed() -> void:
 	print("reloading...", self)
 	get_tree().reload_current_scene()
